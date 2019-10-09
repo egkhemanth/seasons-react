f011ffd0d7190c1d871b35b6fcfaba4bec20cc08
egkhemanth <32567114+egkhemanth@users.noreply.github.com>
2019-10-09 18:25:35 +0530

    Update seasons.js

--- src/components/seasons.js
+++ src/components/seasons.js
@@ -1,32 +1,23 @@
-import './seasons.css'
 import React from 'react'
+const getSeason =(lat,month)=>{
+if(month>2 && month<9){
+    return lat > 0 ? 'summer':'winter' ;
+} else{
+    return lat > 0 ? 'winter':'summer';
+}};
+const SeasonsDisplay=(props)=>{
+    const season=getSeason(props.lat,new Date().getMonth());
+    const text=season==='winter' ?"it's hot pls on AC":"it's cool pls on Heater";
+    const icon=season==='winter'?'Snowflake':'Sun';
 
-const seasonConfig={
-    summer:{
-        text:"hoo it's summer ,on ac plz ",
-        iconName:'sun'
-    },
-    winter:{
-        text:"hoo it's winter, on heater plz ",
-        iconName:'snowflake'
-    }
-};
-const getSeasons= (lat,month)=>{
-    if(month>2 && month<9){
-        return lat>0 ? 'winter' : 'summer'
-    }else{
-        return lat>0 ? 'summer' : 'winter'
-    }
+return(<div>
+    <i className={`${icon}icon`} ></i>
+    <div>{text}</div>
+    <i className={`${icon}icon`} />
+    </div>
+)
 }
 
-const Seasons=(props)=>{
-    const seasons= getSeasons(props.lat,new Date().getMonth())
-   const {text,iconName}=seasonConfig[seasons]
-  
-    return (<div>
-        <i className={`${iconName}icon`}/>
-        <div className='seasons-display' ><h1>{text}</h1></div>
-        <i className={`${iconName}icon`}></i>
-        </div>)
-    }
-export default  Seasons;
+
+
+export default SeasonsDisplay;
